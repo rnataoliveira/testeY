b40cb6c2ffb2b12eb35a5b63581a9426e958e090
Renata Oliveira <renatabels@gmail.com>
2017-09-01 18:47:55 -0300

    Resolução

---
+++ teste/teste/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textoUm = "Esta é a primeira frase";
+            string textoDois = "Esta é a segunda frase";
+
+            string[] palavrasA = textoUm.Split(' ');
+            string[] palavrasB = textoDois.Split(' ');
+
+            IEnumerable<string> palavrasIguais = palavrasA.Intersect(palavrasB);
+
+            Console.WriteLine(string.Join(" ", palavrasIguais));
+        }
+    }
+}
